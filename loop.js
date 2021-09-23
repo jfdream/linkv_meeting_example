c39d354f00e449d7d83aa2c258b577828098c7ea
yangzhigang <yangzhigang@cmcm.com>
2021-09-23 19:12:14 +0800

    更新demo

--- loop.js
+++ loop.js
@@ -26,19 +26,17 @@ button.onclick = function (event) {
   go = !go;
   if (go) {
     let taskId = "taskId" + new Date().getTime();
-    if (os.platform() === "win32") { 
+
     engine.setAVConfig({fps:15, bitrate:1800, min_bitrate:600, videoDegradationPreference:0, videoCaptureWidth:1280, videoCaptureHeight:720, videoEncodeWidth:1280, videoEncodeHeight:720});
-//    engine.setAVConfig(15, 1800, 600, 0, 1280, 720, 1280, 720);
-    } else {
-         engine.setAVConfig({onlyProfile:true, profile:5}); 
-    }
-    engine.StartVVorkRecorder(taskId, "linkv/video", "linkv/image", 10, 2);
+
+    engine.startRecorder(taskId, "/vvork/1427466308800266240/video/", "/vvork/1427466308800266240/img/", 8,  2);
+    console.log("start record");
   }
   else{
-    engine.StopVVorkRecorder();
+    engine.stopRecorder();
+    console.log("stop record");
   }
 }
-
 
 let local_render = new WebGLRender();
 let local_canvas = document.getElementById('local_view');
@@ -54,32 +52,22 @@ remoter_render.setViewMode(LinkVViewMode.AspectFit);
 engine.setISOCountryCode("CN");
 
 // appId,  skStr, userId, callback, 0 为成功
-engine.auth("5291372290", "f5e9cfc87f7d9c41e8b495419e315bc0", "yangyudong", function (code) {
-
+engine.auth("5291372290", "f5e9cfc87f7d9c41e8b495419e315bc0", "yangzg", function (code) {
  	console.log("auth result:" + code);
 });
 
-if (os.platform() === "win32") { 
-  let localDir = "E://work//linkv_rtc_electron//video/";
-  engine.setRecorderConfig("http://lp-api-demo.linkv.fun/v1/utils/presign", "yangzg123456", localDir, 
-  function (taskId, thumbnails, url) {
-    console.log(taskId, url);
-    m3u8_p.innerHTML = "录制结果:" + url + "," + JSON.stringify(thumbnails);
-  });
-} else {
-  let localDir = "/Users/liveme/Desktop/electron_recorder/";
-  engine.setRecorderConfig("https://lp-api-demo.linkv.fun/v1/utils/presign", "yangyudong", localDir, function (taskId, thumbnails, url) {
-    // body...
-    console.log(taskId, url);
-    m3u8_p.innerHTML = "录制结果:" + url + "," + JSON.stringify(thumbnails);
-  });
-}
 
+let localDir = "E://work//linkv_rtc_electron//video/";
+engine.setRecorderConfig("http://lp-api-demo.linkv.fun/v1/utils/presign", "1470ce730f0c62b0e48fffda31944109261fac51", localDir, 
+  function (taskId, thumbnails, url, duration) {
+  console.log(taskId, url, duration);
+  m3u8_p.innerHTML = "录制结果:" + url + "," + JSON.stringify(thumbnails);
+});
 
 console.log("sdkversion:" + result);
 // 建议在鉴权成功之后再加入房间
 if (os.platform() === "win32") 
-  engine.loginRoom("H88000000003232123411", "L12323449384934438491", 1, 0);
+	engine.loginRoom("H88000000003232123411", "L12323449384934438491", 1, 0);
 
 
 engine.on("OnEnterRoomComplete", function (code, list) {
@@ -125,20 +113,33 @@ engine.on("OnCaptureScreenVideoFrame", function (frame, width, height) {
 });
 
 
+
+
 engine.InitCapture(1);
+//engine.InitCapture(2, 1920,1080, {x:0, y:10, width:100, height:100});
 
 let info = engine.GetWindowsList();
+console.log({info});
 
 engine.StartScreenCapture(info[1].id, 15);
 
+
 /*
-engine.initCameraCapture("Logitech HD Webcam C270", "I420", 1280, 720);
+let info = engine.GetVideoCaptureDevice();
+console.log({info});
+
+let info1 = engine.GetCameraResolution(info[0].cameraName);
+console.log({info1});
+
+let info2 = engine.GetCameraColorType(info1[2].width, info1[2].height);
+console.log({info2});
+
+// engine.initCameraCapture("Logitech HD Webcam C270", "I420", 1280, 720);
+engine.initCameraCapture(info[0].cameraName, info2[0].colorFormat, info1[2].width, info1[2].height);
 
 engine.startCapture();
+
 */
-
-
-
 
 
 
